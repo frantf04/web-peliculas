16f504945911b51d650dc219192d4ac46db06bf9
Johan Olmos <frantf04@gmail.com>
2022-02-28 18:02:16 -0400

    agregue nueva funcion para ver mas peliculas

--- src/App.jsx
+++ src/App.jsx
@@ -2,29 +2,19 @@ import styles from "./App.module.css";
 import PeliDetalles from "./pages/PeliDetalles";
 import LandingPage from "./pages/LandingPage";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import { useState } from "react";
+// import { useState } from "react";
 function App() {
-  const [titulo, setTitulo] = useState('movies')
-
-  function setTitu() {
-    if (window.location.href.includes('peli')) {
-      setTitulo('Movies details')
-    } else {
-      setTitulo('Movies')
-      
-    }
-  }
   return (
     <BrowserRouter>
       <div>
         <header  >
           <Link to="/">
-            <h1 onMouseMove={setTitu} className={styles.titulo}>{ titulo }</h1>
+            <h1 className={styles.titulo}>movies</h1>
           </Link>
         </header>
-        <main onMouseMove={setTitu} className={styles.main}>
+        <main className={styles.main}>
           <Routes>
-            <Route exact path="/" element={<LandingPage setTitu={setTitu} />} />
+            <Route exact path="/" element={<LandingPage />} />
             <Route exact path="/peli/:peliId" element={<PeliDetalles />} />
             <Route path="*" element={<div>Error: 404 - Not found</div>} />
           </Routes>
--- src/components/GridPeli.jsx
+++ src/components/GridPeli.jsx
@@ -1,22 +1,38 @@
 import Peliscard from "./Peliscard";
 import styles from "./GridPeli.module.css";
 import { useEffect, useState } from "react";
-import { get } from "../utils/httpClient";
+// import { get } from "../utils/httpClient";
 
-function GridPeli({setTitu}) {
+function GridPeli() {
+  const [pagina, setPagina] = useState(1)
+  const URL_API = "https://api.themoviedb.org/3/movie/popular?";
+  const API_key = "api_key=e3b363ce913a40aa0545b00a3e4522ac";
+
+  const siguiente = () => {
+    setPagina(pagina + 1);
+  }
+  // const anterior = () => {
+  //   setPagina(pagina - 1);
+  // }
   const [pelis, setPelis] = useState([]);
   useEffect(() => {
-    get("/discover/movie").then((data) => {
-      setPelis(data.results);
-    });
-  }, []);
+    fetch(`${URL_API}${API_key}&page=${pagina}`)
+      .then((response) => response.json())
+      .then((data) => {
+        data.results.map((peli) => {
+          return setPelis((e) => [...e, <Peliscard key={peli.id} peli={peli} />]);
+        })
+      });
+  }, [pagina]);
   return (
     <div>
       <ul className={styles.grid_pelis}>
-        {pelis.map((peli) => {
-          return <Peliscard setTitu={setTitu} key={peli.id} peli={peli} />;
-        })}
+        {pelis}
       </ul>
+      <div className={styles.btn_container}>
+        {/* <button onClick={anterior}>Anterior</button> */}
+        <button onClick={siguiente}>Ver mas</button>
+      </div>
     </div>
   );
 }
