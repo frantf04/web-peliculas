401b78e0fbe6fa4e56bae9f7b1062cdd7da9ba62
Johan Olmos <76259577+frantf04@users.noreply.github.com>
2024-06-15 17:57:12 -0400

    Update LandingPage.jsx

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,8 +1,8 @@
 import GridPeli from "../components/GridPeli";
 
 
-function LandingPage({tablaPelis,setTablaPelis,pelis,setPelis}) {
-  return <GridPeli tablaPelis={tablaPelis} setTablaPelis={setTablaPelis} pelis={pelis} setPelis={setPelis} />;
+function LandingPage({tablaPelis,setTablaPelis,pelis,setPelis, pagina, setPagina}) {
+  return <GridPeli pagina={pagina} setPagina={setPagina} tablaPelis={tablaPelis} setTablaPelis={setTablaPelis} pelis={pelis} setPelis={setPelis} />;
 }
 
 export default LandingPage;
